6abe3c76f722b494f3eba4e22c207eb8761a3ca4
Sergey Yelin <elinsn@gmail.com>
2021-06-02 17:49:17 +0300

    More code cleanup

--- src/bankcard_validator_carddata.erl
+++ src/bankcard_validator_carddata.erl
@@ -18,15 +18,6 @@
 
 -include_lib("cds_proto/include/cds_proto_storage_thrift.hrl").
 
--type bank_info() :: #{
-    payment_system := binary(),
-    payment_system_deprecated := dmsl_domain_thrift:'LegacyBankCardPaymentSystem'(),
-    bank_name := binary(),
-    issuer_country := dmsl_domain_thrift:'Residence'() | undefined,
-    category := binary() | undefined,
-    metadata := {_MetaNS :: binary(), map()}
-}.
-
 -type card_data() :: cds_proto_storage_thrift:'PutCardData'().
 -type session_data() :: cds_proto_storage_thrift:'SessionData'().
 -type extra_card_data() :: #{
@@ -41,36 +32,11 @@
     cvc => binary()
 }.
 
--type payment_system() :: binary().
-
--export_type([bank_info/0]).
 -export_type([card_data/0]).
 -export_type([bankcard_data/0]).
 
 %% API
--export([payment_system/1]).
--export([decode_issuer_country/1]).
 -export([build_bankcard_data/3]).
-
--spec payment_system(bank_info()) -> payment_system().
-payment_system(BankInfo) ->
-    maps:get(payment_system, BankInfo).
-
-%% Residence mapping
-%%
--spec decode_issuer_country(binary() | undefined) -> dmsl_domain_thrift:'Residence'() | undefined.
-decode_issuer_country(Residence) when is_binary(Residence) ->
-    try
-        {enum, Variants} = dmsl_domain_thrift:enum_info('CountryCode'),
-        Variant = erlang:list_to_existing_atom(string:to_lower(erlang:binary_to_list(Residence))),
-        element(1, lists:keyfind(Variant, 1, Variants))
-    catch
-        error:badarg ->
-            _ = logger:warning("unknown residence encountered: ~s", [Residence]),
-            erlang:throw({invalid, issuer_country})
-    end;
-decode_issuer_country(undefined) ->
-    undefined.
 
 -spec build_bankcard_data(card_data(), extra_card_data(), session_data() | undefined) -> bankcard_data().
 build_bankcard_data(CardData, ExtraCardData, undefined) ->
